eda3f3db2aee759c959331b871668836047e8769
eqadah <ehab.qadah@iais.fraunhofer.de>
2017-07-24 03:13:50 +0200

    updated the access kafka script & readme file

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # access_kafka_via_ssh_tunel
-This is a shell script to access Kafka cluster via SSH tunnel.
+This is a shell script to access remote Kafka cluster via SSH tunnel.
+
+# Usage:
+`./kafkaTunnel.sh [--remote-machine user@hostname] [--zookeeper host1:port1, ...] [---bootstrap host1:port1, ...]`
+
+* Then the Kafka zookeepers/servers can be accessed on the local machine by the original IPs/ports.
+
+# Reference
+
+This shell scrip is an extension for this [python script](https://github.com/simple-machines/kafka-tunnel_)
--- kafkaTunnel.sh
+++ kafkaTunnel.sh
@@ -1,1 +1,96 @@
 #!/bin/bash
+
+zookeepers=""
+bootstrap_servers=""
+remote_machine=""
+MULT_HOSTS_SEPARATOR=','
+HOST_PORT_SEPARATOR=':'
+hosts_list=""
+host_port=""
+allLocalForwarding="ssh "
+if [ "$#" -ne 6 ]; then
+   echo >&2 \ "USAGE: $0 [--remote-machine user@hostname] [--zookeeper host1:port1, ...] [---bootstrap host1:port1, ...]"
+  exit 1
+fi
+
+while [ $# -gt 0 ]
+do
+    case "$1" in
+        --zookeeper) zookeepers="$2"; shift;;
+	--bootstrap-server) bootstrap_servers="$2"; shift;;
+	--remote-machine) remote_machine="$2"; shift;;
+	--*) echo >&2 \ "USAGE: $0 [--remote-machine user@hostname] [--zookeeper host1:port1, ...] [---bootstrap host1:port1, ...]"
+	    exit 1;;
+	*)  break;;	# terminate while loop
+    esac
+    shift
+done
+
+getListOfHosts(){
+
+IFS=$MULT_HOSTS_SEPARATOR read -r -a hosts_list <<< "$1"
+
+}
+
+splitHostPort(){
+IFS=$HOST_PORT_SEPARATOR read -r -a host_port <<< "$1"
+}
+# Assign loopback IP address
+addLoopbackLink(){
+	
+	sudo ip add a dev lo $1
+}
+
+# Delete loopback IP address
+removeLoopbackLink(){
+	
+	 sudo ip a d dev lo $1
+}
+
+addLoopbackOfHostsList(){	
+	getListOfHosts $1
+	for host_url in "${hosts_list[@]}"
+	do
+		splitHostPort $host_url
+		addLoopbackLink ${host_port[0]}		    
+	done
+}
+
+deleteLoopbackOfHostsList(){
+
+	getListOfHosts $1
+	for host_url in "${hosts_list[@]}"
+	do
+		splitHostPort $host_url
+		removeLoopbackLink ${host_port[0]}
+	    
+	done
+}
+
+addHostToLocalForwarding(){
+
+getListOfHosts $1
+	for host_url in "${hosts_list[@]}"
+	do
+		allLocalForwarding+=" -L "$host_url":"$host_url" "
+			    
+	done
+}
+
+addLoopbackOfHostsList $zookeepers
+addLoopbackOfHostsList $bootstrap_servers
+
+
+
+addHostToLocalForwarding $zookeepers
+
+addHostToLocalForwarding $bootstrap_servers
+
+allLocalForwarding+=$remote_machine
+
+$allLocalForwarding
+
+
+echo "delete all created loopback interfaces"
+deleteLoopbackOfHostsList $zookeepers
+deleteLoopbackOfHostsList $bootstrap_servers
